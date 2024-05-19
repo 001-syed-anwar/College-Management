3eac9692efcfc921b45dcf22ac53ed734b64f803
Syed Anwar <anwarrawna463@gmail.com>
2024-05-19 17:24:00 +0530

    Updated UI Theme

---
+++ CollegeManagement/Migrations/20240519111026_minor update.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class minorupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Staff");
+        }
+    }
+}
